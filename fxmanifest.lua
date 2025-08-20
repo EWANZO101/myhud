df64541d3d083dfeeaa8779d3c6b67eb70c35edb
EWANZO101 <107235474+EWANZO101@users.noreply.github.com>
2025-08-20 15:27:54 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'YourName'
+description 'Simple GPS HUD with /gps command and waypoint distance'
+version '1.0.0'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html'
+}
+
+client_scripts {
+    'client.lua'
+}
